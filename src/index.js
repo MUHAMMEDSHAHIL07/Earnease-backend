097ba45e9fc6c02992396abb9499198ae710cb22
MUHAMMEDSHAHIL07 <161433768+MUHAMMEDSHAHIL07@users.noreply.github.com>
2025-06-27 10:19:25 +0530

    new commit

--- src/index.js
+++ src/index.js
@@ -18,7 +18,6 @@ app.use(cookieParser());
 app.use("/api/auth", authRoute); 
 app.use("/student",studentRoute)
 app.use("/api/employer",employerRoute)
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
