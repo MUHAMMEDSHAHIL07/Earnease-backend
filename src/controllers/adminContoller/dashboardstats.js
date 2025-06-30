ec62ea6879043b0e418c53928ed5b6ef1e1dd78e
MUHAMMEDSHAHIL07 <161433768+MUHAMMEDSHAHIL07@users.noreply.github.com>
2025-06-30 20:32:26 +0530

    admin dashboard stats

--- src/controllers/adminContoller/dashboardstats.js
+++ src/controllers/adminContoller/dashboardstats.js
@@ -1,1 +1,20 @@
-export const dashboardStat 
+import { employerModel } from "../../models/employerSchema"
+import EmployerVerification from "../../models/employerVerifiySchema"
+import { userModel } from "../../models/userSchema"
+
+export const dashboardStat = async(req,res)=>{
+    try{
+        const totalStudent = await userModel.countDocuments()
+        const totalEmployer = await employerModel.countDocuments({isVerified: true})
+        const PendingEmployer = await EmployerVerification.countDocuments({status:"pending"})
+
+        res.status(200).json({
+            totalStudent,
+            totalEmployer,
+            PendingEmployer
+        })
+    }
+    catch(error){
+        return res.status(500).json({message:"internal server error"})
+    }
+}
--- src/routes/adminRoute.js
+++ src/routes/adminRoute.js
@@ -1,5 +1,6 @@
 import express from"express"
 import { approveEmployer, getPendingVerification, getSingleVerification, rejectEmployer } from "../controllers/adminContoller/adminVerificationController.js"
+import { dashboardStat } from "../controllers/adminContoller/dashboardstats.js";
 
 const router = express.Router()
 
@@ -7,5 +8,5 @@ router.get("/employers/pending",getPendingVerification)
 router.get("/employers/pending/:id", getSingleVerification);
 router.patch("/employers/approveEmployer/:id",approveEmployer)
 router.patch("/employers/rejectEmployer/:id",rejectEmployer)
-
+router.get("/dashboard-stats",dashboardStat)
 export default router
